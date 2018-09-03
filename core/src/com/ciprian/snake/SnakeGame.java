0cfc5eb59a160c7caf5df95f2ec5bbfefdb0e906
blinkul <anghelciprian91@gmail.com>
2018-09-03 08:55:43 +0300

    Initial commit

---
+++ core/src/com/ciprian/snake/SnakeGame.java
@@ -0,0 +1,9 @@
+package com.ciprian.snake;
+
+public class SnakeGame extends Game {
+
+	@Override
+	public void create () {
+		setScreen(new GameScreen());
+	}
+}
---
+++ desktop/src/com/ciprian/snake/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ciprian.snake.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ciprian.snake.SnakeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SnakeGame(), config);
+	}
+}
